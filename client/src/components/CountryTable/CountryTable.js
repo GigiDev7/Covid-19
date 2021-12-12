8e27f098118d68712130c50c31c2aab2b143b4ee
Giorgi Vadachkoria <gigivadachkoria@gmail.com>
2021-12-12 20:30:42 +0400

    added countries data table

--- client/src/components/CountryTable/CountryTable.js
+++ client/src/components/CountryTable/CountryTable.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useSelector } from 'react-redux';
+import { useTable } from 'react-table';
+import { COLUMNS } from './columns';
+import styles from './styles.module.css';
 
 const CountryTable = () => {
-  const countries = useSelector((state) => state.countries);
+  const countries = useSelector((state) => state.countries.countries);
   console.log(countries);
 
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => countries, [countries]);
+
+  const tableInstance = useTable({
+    columns,
+    data,
+  });
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    tableInstance;
+
   return (
-    <div>
-      <h1>country table</h1>
-    </div>
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 };
 
---
+++ client/src/components/CountryTable/columns.js
@@ -0,0 +1,18 @@
+export const COLUMNS = [
+  {
+    Header: 'Name',
+    accessor: 'country_id.name.en',
+  },
+  {
+    Header: 'Deaths',
+    accessor: 'deaths',
+  },
+  {
+    Header: 'Recovered',
+    accessor: 'recovered',
+  },
+  {
+    Header: 'Confirmed',
+    accessor: 'confirmed',
+  },
+];
