1b88a23773b3403e5d03f2a3d677d1a6c1fc8898
Giorgi Vadachkoria <gigivadachkoria@gmail.com>
2021-12-14 13:55:24 +0400

    added localization to summary page

--- client/src/components/SummaryCard/SummaryCard.js
+++ client/src/components/SummaryCard/SummaryCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './styles.module.css';
 import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 
 const formatNumber = (num) => {
   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
@@ -9,22 +10,24 @@ const formatNumber = (num) => {
 const SummaryCard = () => {
   const summary = useSelector((state) => state.summary);
 
+  const { t } = useTranslation();
+
   return (
     <div className={styles.container}>
       <div className={styles.card}>
-        <h2>Deaths</h2>
+        <h2>{t('summaryDeaths')}</h2>
         <p>
           {summary?.summary?.deaths && formatNumber(summary.summary.deaths)}
         </p>
       </div>
       <div className={styles.card}>
-        <h2>Recovered</h2>
+        <h2>{t('summaryRecovered')}</h2>
         <p>
           {summary?.summary?.deaths && formatNumber(summary.summary.recovered)}
         </p>
       </div>
       <div className={styles.card}>
-        <h2>Confirmed</h2>
+        <h2>{t('summaryConfirmed')}</h2>
         <p>
           {summary?.summary?.deaths && formatNumber(summary.summary.confirmed)}
         </p>
