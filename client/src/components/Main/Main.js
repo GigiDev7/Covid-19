13a63cdc75fdcb17fcff1d97b59cc6882d5f7fea
Giorgi Vadachkoria <gigivadachkoria@gmail.com>
2021-12-15 14:12:14 +0400

    fixed bug, minor changes

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './styles.module.css';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 
 const Main = () => {
-  const user = JSON.parse(localStorage.getItem('user'));
+  const [isUser, setIsUser] = useState();
+  const user = useSelector((state) => state.auth.userData);
+
+  useEffect(() => {
+    if (user) {
+      setIsUser(true);
+    } else {
+      setIsUser(false);
+    }
+  }, [user]);
 
   const { t } = useTranslation();
 
@@ -16,7 +26,6 @@ const Main = () => {
         transition={{ duration: 1 }}
         animate={{ y: [-200, 0], opacity: 1 }}
       >
-        {/* See Latest Covid 19 Statistics. */}
         {t('mainText')}
         <span>{t('mainSpan')}</span>
       </motion.h1>
@@ -26,10 +35,10 @@ const Main = () => {
         animate={{ y: [100, 0], opacity: 1 }}
         className={styles.link_wrapper}
       >
-        <Link className={styles.link} to={user ? '#' : '/login'}>
+        <Link className={styles.link} to={isUser ? '#' : '/login'}>
           {t('mainBtn1')}
         </Link>
-        <Link className={styles.link} to={user ? '/countries' : '#'}>
+        <Link className={styles.link} to={isUser ? '/countries' : '#'}>
           {t('mainBtn2')}
         </Link>
       </motion.div>
